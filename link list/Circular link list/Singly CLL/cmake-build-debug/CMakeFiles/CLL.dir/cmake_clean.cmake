22e3ac288632547617299869930869997374ff30
GK-SVG <GK32239@gmail.com>
2019-10-02 21:29:21 +0530

    singly circular link list

---
+++ link list/Circular link list/Singly CLL/cmake-build-debug/CMakeFiles/CLL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLL.dir/main.cpp.o"
+  "CLL.pdb"
+  "CLL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ link list/Circular link list/Singly CLL/main.cpp
@@ -0,0 +1,235 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int info;
+    struct node *link;
+};
+
+struct node *start=NULL;
+
+void insertnodelast()
+{
+    struct node *temp,*t;
+    temp=(struct node*) malloc(sizeof(struct node));
+    printf("\n Enter data: ");
+    scanf("%d",&temp->info);
+    temp->link=NULL;
+    if(start==NULL)
+    {
+        start=temp;
+        temp->link=start;
+    }
+    else
+    {
+        t=start;
+        while(t->link!=start)
+        {
+            t=t->link;
+        }
+        t->link=temp;
+        temp->link=start;
+    }
+}
+
+void insertnodefirst()
+{
+    struct node *temp,*t;
+    temp=(struct node*) malloc(sizeof(struct node));
+    printf("\n Enter data: ");
+    scanf("%d",&temp->info);
+    temp->link=NULL;
+    if(start==NULL)
+    {
+        start=temp;
+        temp->link=start;
+    }
+    else
+    {
+        t=start;
+        while (t->link!=start)
+            t=t->link;
+        temp->link=start;
+        start=temp;
+        t->link=temp;
+    }
+
+}
+
+void insertspenode()
+{
+    struct node *temp,*t;
+    int pos,i=1;
+    printf("\n Enter position: ");
+    scanf("%d",&pos);
+    temp=(struct node*) malloc(sizeof(struct node));
+    printf("\n Enter data: ");
+    scanf("%d",&temp->info);
+    t=start;
+    while (i<pos-1)
+    {
+        t=t->link;
+        i++;
+    }
+    temp->link=t->link;
+    t->link=temp;
+}
+
+void deletenodelast()
+{
+    struct node *temp,*pre;
+    if(start==NULL)
+        printf("\n List is empty ");
+    else
+    {
+        temp=start;
+        while (temp->link!=start)
+        {
+            pre=temp;
+            temp=temp->link;
+        }
+        if(temp!=start)
+        {
+            pre->link=start;
+            free(temp);
+        }
+        else
+        {
+            free(temp);
+            start=NULL;
+            printf("\n List is empty");
+        }
+    }
+}
+
+void deletenode1st()
+{
+    struct node *temp,*t;
+    if(start==NULL)
+        printf("\n List is empty ");
+    else
+    {
+       temp=start;
+       t=start;
+       while (t->link!=start)
+           t=t->link;
+       if(t!=start)
+       {
+           t->link = temp->link;
+           start = temp->link;
+           free(temp);
+       }
+       else
+       {
+           free(temp);
+           start=NULL;
+           printf("\n List is empty");
+       }
+    }
+}
+
+void deletespenode()
+{
+    struct node *temp,*next;
+    int i=1,pos;
+    printf("\n Enter position of deletion: ");
+    scanf("%d",&pos);
+    if(start==NULL)
+        printf("\n List is empty");
+    else {
+        temp = start;
+        while (i < pos - 1) {
+            temp = temp->link;
+            i++;
+        }
+        if (temp != start || temp->link!=start)
+        {
+            if(temp==start)
+            {
+                start=temp->link;
+                free(temp);
+            }
+            else
+            {
+                next = temp->link;
+                temp->link = next->link;
+                free(next);
+            }
+        }
+        else
+        {
+            free(temp);
+            start=NULL;
+            printf("\n List is empty");
+        }
+    }
+}
+void display()
+{
+    struct node *temp;
+    if(start==NULL)
+        printf("\n List is empty ");
+    else
+    {
+        temp=start;
+        while (temp->link!=start)
+        {
+            printf("\n%d",temp->info);
+            temp=temp->link;
+        }
+        printf("\n%d",temp->info);
+    }
+}
+
+void menu()
+{
+    int choice;
+    printf("\n1.Insert Node at last");
+    printf("\n2.Tnsert Node at first");
+    printf("\n3.Insert Specific Node");
+    printf("\n4.Delete Last Node");
+    printf("\n5.Delete First Node");
+    printf("\n6.Delete specific Node");
+    printf("\n7.Display");
+    printf("\nEnter your choice: ");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+            insertnodelast();
+            break;
+        case 2:
+            insertnodefirst();
+            break;
+        case 3:
+            insertspenode();
+            break;
+        case 4:
+            deletenodelast();
+            break;
+        case 5:
+            deletenode1st();
+            break;
+        case 6:
+            deletespenode();
+            break;
+        case 7:
+            display();
+            break;
+        default:
+            printf("\n invalid input ");
+    }
+}
+
+int main()
+{
+    int a=1;
+    while(a)
+    {
+        menu();
+        printf("\n Do you want to contineu(0/1) : ");
+        scanf("%d",&a);
+    }
+    return 0;
+}
