5ed33e26b12966220e1f16cf946b0abfad8a213b
GuatamKumar777 <51847650+GK-SVG@users.noreply.github.com>
2019-11-29 17:40:28 +0530

    conting sort source code

---
+++ SortingTechniques/Counting_sort.cpp.cpp
@@ -0,0 +1,59 @@
+// Counting sort in C programming
+#include <stdio.h>
+void countingSort(int array[], int size)
+{
+    int output[10];
+    int max = array[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+    }
+    // The size of count must be at least the (max+1) but
+    // we cannot assign declare it as int count(max+1) in C as
+    // it does not support dynamic memory allocation.
+    // So, its size is provided statically.
+    int count[10];
+    for (int i = 0; i <= max; ++i)
+    {
+        count[i] = 0;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        count[array[i]]++;
+    }
+    for (int i = 1; i <= max; i++)
+    {
+        count[i] += count[i - 1];
+    }
+    for (int i = size - 1; i >= 0; i--)
+    {
+        output[count[array[i]] - 1] = array[i];
+        count[array[i]]--;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = output[i];
+    }
+}
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d  ", array[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int n;
+    printf("\n enter  no. of elements:");
+    scanf("%d",&n);
+    int *array=new int[n];
+    printf("\nenter elements\n");
+    for(int i=0;i<n;i++)
+        scanf("%d",&array[i]);
+    countingSort(array, n);
+    printf("\n Sorted array is \n");
+    printArray(array, n);
+}
